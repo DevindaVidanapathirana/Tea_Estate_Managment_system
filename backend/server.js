f90d16077fa5d6f990e7d1aba9d1fdc8e373ce97
Devinda Vidanapathirana <devinda.vidanapathirana@gmail.com>
2024-05-06 23:36:44 +0530

    p

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json()); // for parsing application/json
+
+
+const URL = process.env.MONGODB_URL;
+// Connect to MongoDB database using Mongoose ORM
+mongoose.connect(URL);
+
+
+// Check connection
+const connection =  mongoose.connection;
+connection.once("open", () => {
+    console.log("Connected to MongoDB!");
+})
+
+
+
+const harvestAndinventoryRouter = require("./routes/harvestAndinventory.js");
+
+
+
+
+app.use("/harvestAndinventory",harvestAndinventoryRouter );
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
